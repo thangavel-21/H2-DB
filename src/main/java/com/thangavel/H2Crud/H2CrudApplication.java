b4cf07d33609879ede21a1f5a28f940c2c0d4e29
Thangavel G <thangavelg@Thangavels-MacBook-Pro.local>
2024-01-21 11:52:39 +0530

    Initial Code Commit

---
+++ src/main/java/com/thangavel/H2Crud/H2CrudApplication.java
@@ -0,0 +1,13 @@
+package com.thangavel.H2Crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2CrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thangavel/H2Crud/H2CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thangavel.H2Crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
